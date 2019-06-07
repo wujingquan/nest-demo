5fdd6ed0285524956655527b731f58d8ab26a474
touch_co <i@wujingquan.com>
2019-06-07 22:17:47 +0800

    add service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,40 +1,35 @@
 import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import { User } from './interfaces/user.interface';
+// import { IUserService } from './interfaces/user-service.interface';
+import {UsersService} from './users.service'
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService:UsersService) {}
+  
   @Get()
   async findAll(): Promise<User[]> {
-    return [
-      {
-        id: 1,
-        name: '小明',
-        age: 18,
-      },
-    ];
+    return await this.usersService.findAll()
   }
 
   @Get(':id')
   async findOne(@Param() params): Promise<User> {
-    return {
-      id: params.id,
-      name: '小明',
-      age: 18
-    }
+    return await this.usersService.findOne(params.id)
   }
 
   @Post()
   async create() {
-
+    return await this.usersService.create()
   }
 
   @Put()
   async edit() {
-
+    return await this.usersService.edit()
   }
 
   @Delete()
   async remove() {
-
+    return await this.usersService.remove()
   }
+  
 }
