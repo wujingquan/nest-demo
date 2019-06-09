b925b8e0035321dfb0aa088aa6d2d39fb53dff61
touch_co <i@wujingquan.com>
2019-06-09 11:05:48 +0800

    add pipe & Validator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,6 +13,7 @@ import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 import { ApiException } from '../common/exceptions/api.exception';
 import { ApiErrorCode } from '../common/enums/api-error-code.enum';
+import { UserIdPipe } from './pipes/user-id.pipe';
 
 @Controller('users')
 export class UsersController {
@@ -24,25 +25,8 @@ export class UsersController {
   }
 
   @Get(':id')
-  async findOne(@Res() res, @Param() params): Promise<User> {
-    let id = parseInt(params.id);
-    if (isNaN(id) || typeof id !== 'number' || id <= 0) {
-      // return res.status(HttpStatus.BAD_REQUEST).send({
-      //   errorCode: 10001,
-      //   errorMessage: '用户编号错误',
-      // });
-      // throw new HttpException('用户编号错误', HttpStatus.BAD_REQUEST)
-      // throw new HttpException({
-      //   errorCode: 10001,
-      //   errorMessage: '用户编号错误',
-      // }, HttpStatus.BAD_REQUEST)
-      throw new ApiException('用户ID无效', ApiErrorCode.USER_ID_INVALID, HttpStatus.BAD_REQUEST)
-    }
-    return res.status(HttpStatus.OK).send({
-      errorCode: 0,
-      errorMessage: '请求成功',
-      data: await this.usersService.findOne(id),
-    });
+  async findOne(@Param('id', new UserIdPipe()) id): Promise<User> {
+    return await this.usersService.findOne(id)
   }
 
   @Post()
